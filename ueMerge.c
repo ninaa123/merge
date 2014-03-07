d303d1cce14737349bd52a2c76ace98497632728
nina <nina@ninas-mbpro.home>
2014-03-07 23:07:52 +0100

    starting

--- ueMerge.c
+++ ueMerge.c
@@ -8,9 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+void mergesort(int * a, int lo, int hi) //int pointer (array), n: anzahl der elemente
+{
+    //überprüfen: if n<2 return
+    //wenn alles ok: do-while schleife
+    //
+}
+
 int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
