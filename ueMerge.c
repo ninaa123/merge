36aab192240aac7dde38b87904a4a91282eb33f2
nina <nina@ninas-mbpro.home>
2014-03-07 22:47:49 +0100

    merge beginning

---
+++ ueMerge.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   ueMerge.c
+ * Author: nina
+ *
+ * Created on 7. März 2014, 22:46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
